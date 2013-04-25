8a4cce8ba3578e2ec3cccd8f804ed625241c052a
Aron Hegyi <ahegyi@gmail.com>
2013-04-25 15:08:38 -0700

    Fixed XPath selectors for flight info and put into instance vars; fixed phantomjs not in PATH by setting absolute

--- phantomjs_multi_destination_flight_search.rb
+++ phantomjs_multi_destination_flight_search.rb
@@ -1,4 +1,5 @@
 require 'erb'
+require 'phantomjs'
 
 class MultiDestinationFlightSearch
   def initialize( start_airport_code, start_date, end_date )
@@ -6,6 +7,10 @@ class MultiDestinationFlightSearch
     @end_date = end_date
     @start_airport_code = start_airport_code
     @destinations = []
+    # the table of results
+    @results_table_xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "GCQ3RC3BLHC", " " ))]'
+    # all the prices
+    @results_prices_xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "GCQ3RC3BIDC", " " ))]'
   end
 
   def add_destination( airport_code, city_name, country_name )
@@ -15,8 +20,10 @@ class MultiDestinationFlightSearch
   def cheapest_flights
     erb = ERB.new( File.read( 'google_api.js.erb' ) )
     javascript_file = erb.result( binding )
+    
     File.open('countries.js', 'w') {|f| f.write(javascript_file) }
-    results = `phantomjs countries.js`
+    
+    results = `#{phantomjs_path} countries.js`
     results.split( "\n" ).sort do |a, b|
       convert_price_to_integer( a.split( ' , ' ).first ) <=> convert_price_to_integer( b.split( ' , ').first )
     end.join( "\n" )
@@ -36,8 +43,10 @@ class MultiDestinationFlightSearch
 
   def url( airport_code )
     "http://www.google.com/flights/#search;f=#{@start_airport_code};t=#{airport_code};d=#{@start_date};r=#{@end_date}"
-    
   end
-  
+
+  def phantomjs_path
+    Phantomjs.path
+  end
   
 end
